7603bfc96bb4b906c082175a807ae97865c12b65
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-04-25 11:30:33 +0200

    test, doesn't work

--- src/main/java/fr/iamacat/mycoordinatesmods/MyCoordinatesMods.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/MyCoordinatesMods.java
@@ -1,13 +1,19 @@
 package fr.iamacat.mycoordinatesmods;
 
+import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import fr.iamacat.mycoordinatesmods.proxy.CommonProxy;
 import fr.iamacat.mycoordinatesmods.utils.Reference;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.settings.KeyBinding;
+import org.lwjgl.input.Keyboard;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSION)
 public class MyCoordinatesMods {
@@ -19,17 +25,26 @@ public class MyCoordinatesMods {
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event) {
     }
-    public static class WorldLoadHandler {
-
-    }
 
     @Mod.EventHandler
     public static void postInit(FMLPostInitializationEvent event) {
     }
 
-
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.registerRenders();
+
+        // Add your custom key binding
+        KeyBinding[] keyBindings = {new KeyBinding("Show Coordinates", Keyboard.KEY_C, "MyCoordinatesMods")};
+        ClientRegistry.registerKeyBinding(keyBindings[0]);
+    }
+
+    @Mod.EventHandler
+    public void onTick(TickEvent.RenderTickEvent event) {
+        // Show the GUI when the "Show Coordinates" key is pressed
+        if (Keyboard.isKeyDown(Keyboard.KEY_C) && Minecraft.getMinecraft().currentScreen == null) {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiCoordinates());
+            System.out.println("onTick");
         }
     }
+}
