44795b944fd9e1f97452e12bb35878a8dd5dff9d
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-06-05 19:15:12 +0200

    Readd !minecraft.gameSettings.showDebugInfo check

--- src/main/java/fr/iamacat/mycoordinatesmods/eventhandler/CoordinatesEventHandler.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/eventhandler/CoordinatesEventHandler.java
@@ -18,7 +18,7 @@ public class CoordinatesEventHandler {
             return;
         }
         Minecraft minecraft = Minecraft.getMinecraft();
-        if (showCoordinates) {
+        if (showCoordinates && !minecraft.gameSettings.showDebugInfo ) {
             FontRenderer fontRenderer = minecraft.fontRenderer;
             ScaledResolution scaledResolution = event.resolution;
             int xCoord, yCoord;
