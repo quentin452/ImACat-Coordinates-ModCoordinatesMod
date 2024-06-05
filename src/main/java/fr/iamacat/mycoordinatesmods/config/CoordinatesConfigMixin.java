b4afa56dfaa1ce3357f583828a591f6bd1627089
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-06-05 10:31:49 +0200

    Remove unused config

--- src/main/java/fr/iamacat/mycoordinatesmods/config/CoordinatesConfigMixin.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/config/CoordinatesConfigMixin.java
@@ -7,9 +7,9 @@ import fr.iamacat.mycoordinatesmods.Tags;
 @Config(modid = Tags.MODID)
 public class CoordinatesConfigMixin {
 
-    @Config.Comment("Disabling Teleporter wand from Farlanders causing infinite loop (https://github.com/quentin452/privates-minecraft-modpack/issues/917)")
-    @Config.DefaultBoolean(true)
-    @Config.RequiresWorldRestart
-    public static boolean enableMixinItemMysticWandTeleport;
+   // @Config.Comment("Disabling Teleporter wand from Farlanders causing infinite loop (https://github.com/quentin452/privates-minecraft-modpack/issues/917)")
+   // @Config.DefaultBoolean(true)
+    //@Config.RequiresWorldRestart
+    //public static boolean enableMixinItemMysticWandTeleport;
 
 }
