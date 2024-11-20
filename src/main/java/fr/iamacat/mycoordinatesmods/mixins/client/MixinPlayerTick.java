97d340c074c30d3291c93eaf9f2417f43ab54d58
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-11-20 02:04:22 +0000

    Fix lags caused by onMyUpdate of MixinPlayerTick

--- src/main/java/fr/iamacat/mycoordinatesmods/mixins/client/MixinPlayerTick.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/mixins/client/MixinPlayerTick.java
@@ -25,50 +25,62 @@ public class MixinPlayerTick {
     private long imACat_Coordinates_ModCoordinatesMod$lastToggleTime;
     @Unique
     private static final long imACat_Coordinates_ModCoordinatesMod$TOGGLE_DELAY = 100; // ms
+    @Unique
+    private boolean isToggleKey1Pressed = false;
+    @Unique
+    private boolean isToggleKey2Pressed = false;
 
     @Inject(method = "onUpdate", at = @At("HEAD"))
     public void onMyUpdate(CallbackInfo ci) {
         if (Minecraft.getMinecraft().currentScreen != null) {
             return;
         }
-        imACat_Coordinates_ModCoordinatesMod$handleToggleKey(
-            toggleKeyBinding,
-            () -> CoordinatesEventHandler.showCoordinates = !CoordinatesEventHandler.showCoordinates);
-        imACat_Coordinates_ModCoordinatesMod$handleToggleKey(toggleKeyBinding2, () -> {
-            switch (CoordinatesConfig.hudPosition) {
-                case "top_left":
-                    CoordinatesConfig._Position = "top_right";
-                    break;
-                case "top_right":
-                    CoordinatesConfig._Position = "bottom_left";
-                    break;
-                case "bottom_left":
-                    CoordinatesConfig._Position = "bottom_right";
-                    break;
-                case "bottom_right":
-                    CoordinatesConfig._Position = "top_left";
-                    break;
-                default:
-                    break;
-            }
-            CoordinatesConfig.hudPosition = CoordinatesConfig._Position;
-            CoordinatesConfig.config
-                .get(
-                    CATEOGY_BIOMES,
-                    "HUD Position(Possible config : top_left , top_right , bottom_left , bottom_right)",
-                    CoordinatesConfig._Position)
-                .set(CoordinatesConfig._Position);
-            if (CoordinatesConfig.config.hasChanged()) {
-                CoordinatesConfig.config.save();
-            }
-        });
-    }
 
-    @Unique
-    private void imACat_Coordinates_ModCoordinatesMod$handleToggleKey(KeyBinding keyBinding, Runnable action) {
-        if (Keyboard.isKeyDown(keyBinding.getKeyCode()) && System.currentTimeMillis() > imACat_Coordinates_ModCoordinatesMod$lastToggleTime + imACat_Coordinates_ModCoordinatesMod$TOGGLE_DELAY) {
-            imACat_Coordinates_ModCoordinatesMod$lastToggleTime = System.currentTimeMillis();
-            action.run();
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime > imACat_Coordinates_ModCoordinatesMod$lastToggleTime + imACat_Coordinates_ModCoordinatesMod$TOGGLE_DELAY) {
+            imACat_Coordinates_ModCoordinatesMod$lastToggleTime = currentTime;
+
+            if (Keyboard.getEventKey() == toggleKeyBinding.getKeyCode()) {
+                isToggleKey1Pressed = Keyboard.getEventKeyState();
+            }
+
+            if (Keyboard.getEventKey() == toggleKeyBinding2.getKeyCode()) {
+                isToggleKey2Pressed = Keyboard.getEventKeyState();
+            }
+
+            if (isToggleKey1Pressed) {
+                CoordinatesEventHandler.showCoordinates = !CoordinatesEventHandler.showCoordinates;
+            }
+
+            if (isToggleKey2Pressed) {
+                switch (CoordinatesConfig.hudPosition) {
+                    case "top_left":
+                        CoordinatesConfig._Position = "top_right";
+                        break;
+                    case "top_right":
+                        CoordinatesConfig._Position = "bottom_left";
+                        break;
+                    case "bottom_left":
+                        CoordinatesConfig._Position = "bottom_right";
+                        break;
+                    case "bottom_right":
+                        CoordinatesConfig._Position = "top_left";
+                        break;
+                    default:
+                        break;
+                }
+                CoordinatesConfig.hudPosition = CoordinatesConfig._Position;
+                CoordinatesConfig.config
+                    .get(
+                        CATEOGY_BIOMES,
+                        "HUD Position(Possible config : top_left , top_right , bottom_left , bottom_right)",
+                        CoordinatesConfig._Position)
+                    .set(CoordinatesConfig._Position);
+                if (CoordinatesConfig.config.hasChanged()) {
+                    CoordinatesConfig.config.save();
+                }
+            }
         }
     }
 }
