b2c54284926ae9b0cb20a3559101a5476e9ef912
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-01-01 03:07:55 +0000

    ported to 1.18.2 fabric

--- src/main/java/fr/iamacat/mycoordinatesmods/utils/Tags.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/utils/Tags.java
@@ -5,7 +5,7 @@ public class Tags {
     public static final String MODID = "mycoordinatesmods";
     public static final String MODNAME = "My Coordinates Mod";
     public static final String VERSION = "0.2";
-    public static final String MCVERSION = "1.18.1";
+    public static final String MCVERSION = "1.18.2";
     public static final String GROUPNAME = "fr.iamacat.mycoordinatesmods";
     public static final String CLIENTPROXY = "fr.iamacat.mycoordinatesmods.proxy.ClientProxy";
     public static final String SERVERPROXY = "fr.iamacat.mycoordinatesmods.proxy.CommonProxy";
