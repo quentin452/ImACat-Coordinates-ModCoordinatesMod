7084c271de29593e4376d88621aae908c3a59894
quentin452 <42176772+quentin452@users.noreply.github.com>
2025-01-01 02:32:14 +0000

    fix cardinal points calculation

--- src/main/java/fr/iamacat/mycoordinatesmods/eventhandler/CoordinatesEventHandler.java
+++ src/main/java/fr/iamacat/mycoordinatesmods/eventhandler/CoordinatesEventHandler.java
@@ -75,8 +75,7 @@ public class CoordinatesEventHandler {
     private static final String[] cardinalPoints = { "S", "SW", "W", "NW", "N", "NE", "E", "SE" };
 
     private static char getCardinalPoint(float yaw) {
-        yaw = (yaw + 360) % 360;
-        int index = Math.round((yaw + 22.5f) / 45f) & 7;
+        int index = Math.round(yaw / 45f) & 7;
         return cardinalPoints[index].charAt(0);
     }
 }
